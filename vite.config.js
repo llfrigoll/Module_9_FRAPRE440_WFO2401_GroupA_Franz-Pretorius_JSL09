597e336dc22f043940306bc576b352c1886dbdb5
Franz Pretorius <franzj.pretorius.2003@gmail.com>
2024-09-18 10:37:35 +0200

    Updated config file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
-import {defineConfig} from 'vite'
+// import {defineConfig} from 'vite'
 
 
-export default defineConfig({
-	plugins: []	
-})
+// export default defineConfig({
+// 	plugins: []	
+// })
+
+// vite.config.js
+
+export default {
+	build: {
+	  target: 'es2019' // or 'es2018', 'es2017', etc. as long as it does not include top-level await
+	}
+  }
